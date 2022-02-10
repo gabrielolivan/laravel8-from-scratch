543e1852747c0a26bca813b04af94f867eeafad1
Gabriel.Way2 <gabriel.neves@way2.com.br>
2022-02-10 17:59:12 -0300

    aula 34

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,14 @@
 <x-layout>
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{$post->slug}}">
-                {!! $post->title !!}
-                </a>
-            </h1>
-            
-            <p>
-                By 
-                <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
-                in 
-                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
+    @include('_post-header')    
 
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
-    @endforeach
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        
+        @if ($posts->count())
+        <x-posts-grid :posts="$posts" />
+        @else
+        <p>No posts yet. Please check back later</p>
+        @endif
+
+    </main>
+
 </x-layout>
